d56a5bc46de13344f7f0d025cf23ea0f814e52c1
user <pavan.hp001@gmail.com>
2018-06-15 07:44:16 +0530

    First initiol commit

---
+++ Spring-proctice/src/main/java/com/pavan/Springproctice/SpringProcticeApplication.java
@@ -0,0 +1,12 @@
+package com.pavan.Springproctice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProcticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProcticeApplication.class, args);
+	}
+}
