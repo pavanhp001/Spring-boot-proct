aa2f4ddf1ebf0a0ac5a0525b18460545d98fb47a
user <pavan.hp001@gmail.com>
2019-03-02 08:44:50 +0530

    Commiting for basic boot batch application

---
+++ springBootBatch_basic/src/main/java/ch/javaee/springBootBatch/Application.java
@@ -0,0 +1,9 @@
+package ch.javaee.springBootBatch;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String args[]) {
+        SpringApplication.run(BatchConfiguration.class, args);
+    }
+}
